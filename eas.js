8513d489d1a7dca1cff5eb68c79ec38f9f3813ac
TomL <youtom.lee17@gmail.com>
2021-11-07 23:11:28 +0800

    Added button to clear grids and make new grids of size as user input; Made random colors when sketching

--- eas.js
+++ eas.js
@@ -1,22 +1,55 @@
 const container = document.querySelector('.container');
 
-function makeGrids(rows, cols) {
+function makeGrids(rowsCols) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
-    grid.style.width = 100 / cols + '%';
-    grid.style.paddingTop = 100 / rows + '%';
-    // grid.textContent = '123';
-    for (let i = 0; i < (rows * cols); i++) {
+    grid.style.width = 100 / rowsCols + '%';
+    grid.style.paddingTop = grid.style.width;
+    for (let i = 0; i < (rowsCols ** 2); i++) {
         container.appendChild(grid.cloneNode(true));
     }
 }
 
-makeGrids(16, 16);
+makeGrids(16);
 
-const grids = Array.from(document.querySelectorAll('.grid'));
-//console.log(grids);
-grids.forEach(grid => grid.addEventListener('mouseover', (e) => {
-    console.log(grid);
-    e.target.classList.add('grid-hover');
-}))
+
+function randomColor(el) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
+
+let grids = undefined;
+function mouseSketch() {
+    grids = Array.from(document.querySelectorAll('.grid'));
+    grids.forEach(grid => grid.addEventListener('mouseover', (e) => {
+        //e.target.classList.add('grid-hover');
+        e.target.style.backgroundColor = randomColor();
+    }))
+}
+
+mouseSketch();
+
+function remakeGrid() {
+    container.textContent = '';
+    let newGrid = +prompt('Enter new grid\s number of squares per side. (Max=100)', '16');
+    for (i = 0; i < 1;) {
+        if (newGrid <= 100 && newGrid > 1 && Number.isInteger(newGrid)) {
+            makeGrids(newGrid);
+            i++;
+        }
+        else newGrid = +prompt('Please enter a number less than or equal to 100.', '16');
+    }
+}
+
+const btn = document.querySelector('#clear');
+
+btn.addEventListener('click', function () {
+    grids.forEach(grid => grid.classList.remove('grid-hover'));
+    remakeGrid();
+    mouseSketch();
+})
+
+
 
